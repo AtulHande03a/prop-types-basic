eb803271fa4f2d813227479d0b2d0614e4123a40
Atul Kumar Hande <handeatul03a@gmail.com>
2021-10-03 19:31:53 +0530

    using custom hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import UseFetch from "../src/useFetch/fetch-example";
+
 import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <UseFetch />
     </div>
   );
 }
